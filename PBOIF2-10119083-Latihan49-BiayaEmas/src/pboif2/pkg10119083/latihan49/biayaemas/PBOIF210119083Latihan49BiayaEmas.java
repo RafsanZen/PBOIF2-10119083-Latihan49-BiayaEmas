2680e514281dba45c95ec4d69d8ece7e5a0dc724
Rafsan Zen M <73044188+RafsanZen@users.noreply.github.com>
2020-11-13 23:05:25 +0700

    Add files via upload

---
+++ PBOIF2-10119083-Latihan49-BiayaEmas/src/pboif2/pkg10119083/latihan49/biayaemas/PBOIF210119083Latihan49BiayaEmas.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119083.latihan49.biayaemas;
+
+/**
+ *
+ * @Nama     : Rafsan Zen Mustaofa
+ * NIM       : 10119083
+ * Kelas     : IF2
+ * Deskripsi : Menampilkan program untuk biaya emas
+ */
+public class PBOIF210119083Latihan49BiayaEmas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Emas emas = new Emas(570000,15.7);
+        
+        emas.tampil();
+        System.out.println("==========================================");
+        System.out.println("Developed by : Rafsan Zen mustaofa");
+    }
+    
+}
